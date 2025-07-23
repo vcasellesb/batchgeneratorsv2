7c1523cf3fcd87b00cbc429b469575fefe8c3df1
Vicent Caselles <vcasellesb@uoc.edu>
2025-07-23 19:01:53 +0200

    new special transform

---
+++ batchgeneratorsv2/transforms/longitudinal/ciao.py
@@ -0,0 +1,42 @@
+from typing import Iterable
+
+import torch
+
+from batchgeneratorsv2.transforms.base.basic_transform import ImageOnlyTransform
+
+
+class CiaoBaseline(ImageOnlyTransform):
+    """
+    Removes baseline -- this is thought for trying to make a network learn to work in both
+    longitudinal and cross-sectional settings.
+    """
+    def __init__(
+            self,
+            p_per_channel: float | Iterable[float]
+        ):
+
+        self.p_per_channel = torch.tensor(p_per_channel, dtype=float)
+
+
+    def get_parameters(self, **data_dict):
+        shape = data_dict['image'].shape
+        apply_per_channel = torch.rand(shape[0]) < self.p_per_channel
+        return {'apply_per_channel': apply_per_channel}
+
+
+    def _apply_to_image(self, img: torch.Tensor, **params):
+        if sum(params.get('apply_per_channel')) > 0:
+            assert sum(params.get('apply_per_channel')) == 1
+            this_channel = list(params.get('apply_per_channel')).index(True)
+            print('heyyyyy setting baseline to channel %i' % this_channel)
+        img[params.get('apply_per_channel')] *= 0
+        return img
+    
+
+if __name__ == "__main__":
+    x = torch.randn((2, 128, 128, 128))
+    ciao = CiaoBaseline([0, 1])
+    out = ciao(image=x)
+
+    assert x[1].sum() == 0. and x[1].max() == 0.
+    assert x[0].max() != 0 and x[0].min() != 0
